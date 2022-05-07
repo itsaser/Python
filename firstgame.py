0a3c1f3704053c6a8ffc38d9e1cc2473f4eab097
itsaser <milordaser@gmail.com>
2022-05-07 19:54:56 -0400

    updating game first build

--- firstgame.py
+++ firstgame.py
@@ -1,1 +1,43 @@
-# This is the beginning of a new journey.
+# starting screen
+print("Welcome Stranger.")
+
+#transition from starting screen to info screen
+input("Press the \"Enter\" key to get started. ")
+# info screen
+age = int(input("Enter age: "))
+
+def ages(age):
+    if age <= 30:
+     print("You're a baby! Cool, lets get started.") 
+    else:
+        print("Let's get started!")
+ages(age)
+
+name = str(input("What is your name? "))
+
+print("You're probably wondering \"what the hell is this?\"", "Well", name, "this is a 10 question based game. I want to see if I know what I'm doing.")
+print(" ")
+
+question_one = str(input("What is CPU? "))
+print(" ")
+
+score = 0
+def questions(question_one):
+    while True:
+        try:    
+            for question in question_one:
+                global score
+                if question_one == "central processing unit":
+                    print("Correct!")
+                    score += 1
+                    question_one = True
+                    break
+                else:
+                    print("try again")
+                    break
+        except ValueError:
+            print("Incorrect. Please try again. Make sure you're using all lowercase.")
+            break            
+
+questions(question_one)
+print("score:", score)
